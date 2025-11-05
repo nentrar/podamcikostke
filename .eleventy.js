fb5ddac8908f5d493a80bc35f27cab18410d09e5
nentrar <nentrar.mail@gmail.com>
2025-11-05 10:21:45 +0100

    Add feature to jump between posts by auto

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,17 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("style.css");
-  eleventyConfig.addPassthroughCopy("assets/images");
+    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("style.css");
+    eleventyConfig.addCollection("posts", function(collectionApi) {
+        return collectionApi.getFilteredByTag("post").sort((a, b) => {
+            return b.date - a.date; 
+        });
+    });
+    
+    return {
+        dir: {
+            input: ".",        
+            output: "_site",
+            includes: "_includes"
+        }
+    };
 };
