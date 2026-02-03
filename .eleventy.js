c557306ad9ff9ae398f96431f044d4668ade8bc5
nentrar <nentrar.mail@gmail.com>
2026-02-03 19:22:59 +0100

    fix date to pl

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,21 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("style.css");
+
     eleventyConfig.addCollection("posts", function(collectionApi) {
         return collectionApi.getFilteredByTag("post").sort((a, b) => {
             return b.date - a.date; 
         });
     });
-    
+
+    eleventyConfig.addFilter("datePL", (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: "utc" })
+            .setLocale("pl")
+            .toFormat("d LLLL yyyy");
+    });
+
     return {
         dir: {
             input: ".",        
